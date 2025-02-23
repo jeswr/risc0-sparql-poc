7eb96c24385cf0a983be9f8e411b14020afbf422
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-02-23 12:35:58 +0000

    chore: commit non-core changes

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -14,54 +14,12 @@
 
 #![no_main]
 
-use json_core::{I, BlankNode, run, I2, I2Content};
+use json_core::I2;
 use risc0_zkvm::guest::env;
-use oxrdf::{NamedNode, Quad, Literal};
 
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    let data: String = env::read();
-    let query: String = env::read();
-    let quads: I = env::read();
-
-    if quads.result_string != "boo" {
-        panic!("Expected 'boo' but got {:?}", quads.result_string);
-    }
-
-    let i2: I2 = env::read();
-
-    match i2 {
-        I2(I2Content::A) => (),
-        _ => panic!("Expected 'A' but got {:?}", i2),
-    }
-
-    let _q: NamedNode = env::read();
-
-    if _q.as_str() != "http://example.com/subject" {
-        panic!("[NamedNode] Expected 'http://example.com/subject' but got {:?}", _q.as_str());
-    }
-
-    // let _q2: BlankNode = env::read();
-
-    // if "a" != "b" {
-    //     panic!("[BlankNode] Expected 'a' but got {:?}", "b");
-    // }
-
-    // if _q2.as_str() != "http://example.com/subject" {
-    //     panic!("[BlankNode] Expected 'http://example.com/subject' but got {:?}", _q2.as_str());
-    // }
-
-    // let _q3: Literal = env::read();
-
-    if quads.result_string != "boo" {
-        panic!("Expected 'boo' but got {:?}", quads.result_string);
-    }
-
-    // if quads.result_string != "boo2" {
-    //     panic!("Expected 'boo2' but got {:?}", quads.result_string);
-    // }
-    
-    let out = run(&data, &query, &quads);
-    env::commit(&out);
+    let _i2: I2 = env::read();
+    env::commit(&"complete");
 }
